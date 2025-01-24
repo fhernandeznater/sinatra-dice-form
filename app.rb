363c09d8fee19c254330ea13c0a3398ac97ce380
Federico Hernandez Nater <166563763+fhernandeznater@users.noreply.github.com>
2025-01-24 20:21:55 +0000

    Removed unnecessary code—we're starting from scratch with this project

--- app.rb
+++ app.rb
@@ -2,21 +2,9 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  erb(:homepage)
+  "Main Page"
 end
 
-get("/dice/:number_of_dice/:how_many_sides") do
-  @num_dice = params.fetch("number_of_dice").to_i
-
-  @sides = params.fetch("how_many_sides").to_i
-  
-  @rolls = []
-
-  @num_dice.times do
-    die = rand(1..@sides)
-
-    @rolls.push(die)
-  end
-
-  erb(:flexible)
+get("/process_roll") do
+  "Howdy"
 end
